b8c6261a6f5917483fefa892e6eb64ddb8d36153
OliTaylor32 <oli.taylor.2001@gmail.com>
2018-10-30 17:25:02 +0000

    Adding Stamina

---
+++ Oli8155/Assets/Scripts/Aiming.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public float Range = 10.0f;
+    Rigidbody2D rigidbody2D;
+    public bool selected = false;
+    public float distanceTravelled = 0;
+    Vector3 startingPosition;
+    private float x;
+    private float y;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        distanceTravelled = Vector3.Distance(startingPosition, transform.position);
+
+        if (distanceTravelled > stamina)
+        {
+            selected = false;
+
+        }
+
+        if (selected == true)
+        {
+            float x = Input.GetAxis("Horizontal") * 0.25f;
+            float y = Input.GetAxis("Vertical") * 0.25f;
+            Movement(rigidbody2D, x, y, speed);
+        }
+
+        if (selected == false)
+        {
+            x = 0;
+            y = 0;
+        }
+
+    }
+}
--- Oli8155/Assets/Scripts/TopDownCharacterController2D.cs
+++ Oli8155/Assets/Scripts/TopDownCharacterController2D.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 public class TopDownCharacterController2D : MonoBehaviour {
 
     public float speed = 5.0f;
+    public float stamina = 10.0f;
     Rigidbody2D  rigidbody2D;
-
-        // Use this for initialization
-	void Start () {
-        rigidbody2D = GetComponent<Rigidbody2D>();	
-	}
+    public bool selected = true;
+    public float distanceTravelled = 0;
+    Vector3 startingPosition;
+    private float x;
+    private float y;
+    // Use this for initialization
+    void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
+}
 	
-	// Update is called once per frame
-	void Update () {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
+    private static void Movement(Rigidbody2D rigidbody2D, float x, float y, float speed)
+    {
         rigidbody2D.velocity = new Vector2(x, y) * speed;
         rigidbody2D.angularVelocity = 0.0f;
-	}
+    }
+	// Update is called once per frame
+	void Update () {
+
+        distanceTravelled = Vector3.Distance(startingPosition, transform.position);
+
+        if (distanceTravelled > stamina)
+        {
+            selected = false;
+        }
+
+        if (selected == true)
+        {
+            float x = Input.GetAxis("Horizontal") * 0.25f;
+            float y = Input.GetAxis("Vertical") * 0.25f;
+            Movement(rigidbody2D, x, y, speed);
+        }
+
+        if (selected == false)
+        {
+            x = 0;
+            y = 0;
+        }
+
+        
+    }
 }
