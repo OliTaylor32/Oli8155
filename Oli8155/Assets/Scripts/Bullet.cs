3f2e7f4e27e0c3923333214e5ce99cfb86e52f94
OliTaylor32 <oli.taylor.2001@gmail.com>
2018-11-06 17:21:49 +0000

    Creating a Bullet

--- Oli8155/Assets/Scripts/Bullet.cs
+++ Oli8155/Assets/Scripts/Bullet.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
+    public float moveSpeed = 100.0f;
+    public int damage = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(transform.up * moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.SendMessage("Attacked", damage, SendMessageOptions.DontRequireReceiver);
+        Die();
+    }
+
+    private void OnBecomeInvisible()
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
