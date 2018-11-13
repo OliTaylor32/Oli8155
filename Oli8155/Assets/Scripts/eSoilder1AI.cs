616b4106a1dd8b75a85532d9101e285b7bce8113
Oli <oli.taylor.2001@gmail.com>
2018-11-13 14:04:50 +0000

    Begun on first Enemy's AI

---
+++ Oli8155/Assets/Scripts/eSoilder1AI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eSoilder1AI : MonoBehaviour {
+    public static string defence = "eSoilder1"; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    private static void Attacked(int damage)
+    {
+       GameObject.Find("TurnMaster").SendMessage("Battle", defence, SendMessageOptions.DontRequireReceiver);
+    }
+}
