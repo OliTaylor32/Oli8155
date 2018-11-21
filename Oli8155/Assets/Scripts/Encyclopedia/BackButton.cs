e5193d1778ccfc2a307847f0592b587453c27041
Oli <oli.taylor.2001@gmail.com>
2018-11-21 00:17:58 +0000

    Clean-up & Encyclopedia
    Deleted un-needed assets and added in the encyclopedia menu, now accessable through the main menu.

---
+++ Oli8155/Assets/Scripts/Encyclopedia/BackButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start () {
+ 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        while (transform.localScale.x < 0.8)
+        {
+            transform.localScale += new Vector3(0.1F, 0.1f, 0.1f);
+        }
+            
+    }
+
+    void OnMouseExit()
+    {
+        while (transform.localScale.x > 0.8)
+        {
+            transform.localScale += new Vector3(-0.1F, -0.1f, -0.1f);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Application.LoadLevel("Main Menu");
+    }
+
+}
