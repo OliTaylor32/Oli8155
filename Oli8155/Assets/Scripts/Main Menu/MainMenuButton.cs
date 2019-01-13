7e4dea40d8ef6a74ef6285ab65e7e2d34d208f00
Oli <oli.taylor.2001@gmail.com>
2019-01-13 00:49:09 +0000

    Finished game over screen
    The player has the choice to either return to the main menu or retry the level.

---
+++ Oli8155/Assets/Scripts/Main Menu/MainMenuButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        while (transform.localScale.x < 0.9)
+        {
+            transform.localScale += new Vector3(0.1F, 0.1f, 0.1f);
+        }
+
+    }
+
+    void OnMouseExit()
+    {
+        while (transform.localScale.x > 0.9)
+        {
+            transform.localScale += new Vector3(-0.1F, -0.1f, -0.1f);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Application.LoadLevel("Main Menu");
+    }
+}
